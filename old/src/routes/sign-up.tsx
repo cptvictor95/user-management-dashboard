47de2ecf4ffbf3ce234176631f9fd27411c55283
Victor Pudo <cpt.victor@hotmail.com>
2025-06-26 22:26:02 -0300

    refac: remove unused code

--- old/src/routes/sign-up.tsx
+++
@@ -1,6 +0,0 @@
-import { SignUp } from "@/components/auth/sign-up";
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/sign-up")({
-  component: SignUp,
-});
