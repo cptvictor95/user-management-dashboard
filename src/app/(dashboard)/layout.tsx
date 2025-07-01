6bce16359105982551b4560fe00a49dbc8df71d4
Victor Pudo <cpt.victor@hotmail.com>
2025-06-30 21:46:52 -0300

    refac: remove unused page and route

--- src/app/(dashboard)/layout.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  console.log("🏠 Dashboard Layout rendered:", new Date().toISOString());
-  return <div>{children}</div>;
-};
-
-export default Layout;
